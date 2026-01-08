dd411947e3eb533b88aeaf5ec49d1dab3e1be461
eleonorsilva96 <eleonor.uxui@gmail.com>
2026-01-08 23:41:35 +0000

    set colors and font

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,3 +1,6 @@
 import { Quicksand } from 'next/font/google';
 
-export const quicksand = Quicksand({ subsets: ['latin'] })
+export const quicksand = Quicksand({ 
+    subsets: ['latin'],
+    variable: "--font-quicksand" 
+})
