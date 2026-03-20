8b0fd21c32ed7ee0129d0e028fdbac43d811a6da
eleonorsilva96 <eleonor.uxui@gmail.com>
2026-03-20 21:49:59 +0000

    feat: fetch services page data

--- app/(my-app)/services/[slug]/page.tsx
+++ app/(my-app)/services/[slug]/page.tsx
@@ -1,16 +1,15 @@
 import { getPayload } from "payload";
 import config from '@payload-config';
-
+import { Metadata } from 'next'
 import { unstable_cache } from "next/cache";
-
-import { Service } from "@/payload-types";
+import { notFound } from 'next/navigation';
 
 import SectionContent from "@/app/(my-app)/ui/section-content";
 
 async function getServiceData(slug: string) {
   // using the Payload Local API is a better option because the Next.js frontend (Server Components) 
   // and Payload CMS backend live in the exact same Node.js server. This delivers faster responses just by calling a function
-  // and completely eliminates the need for HTTP network requests.
+  // and drops the need for HTTP network requests.
   const payload = await getPayload({ config });
 
   const result = await payload.find({
@@ -35,9 +34,34 @@ export const getCachedServiceBySlug = unstable_cache(
   // tag name to eventually clear it when data changes on the db
   {
     tags: ['collection_services'], 
-    revalidate: 3600, // auto-revalidate every hour
+    revalidate: 86400, // auto-revalidate every 24 hours
   }
 );
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+
+  const service = await getCachedServiceBySlug(slug);
+
+  if (!service) {
+    return { title: 'Service Not Found' }
+  }
+  
+  return {
+    title: service.meta?.title || service.title,
+    description: service.meta?.description,
+    openGraph: {
+      title: service.meta?.title || service.title,
+      // add condition to omit description if it doesn't exist
+      ...(service.meta?.description && { description: service.meta.description }), 
+      images: typeof service.meta?.image === 'object' && service.meta?.image?.url ? [{ url: service.meta.image.url }] : [],
+    },
+  }
+}
 
 export default async function ServicesPage({
   params,
@@ -47,10 +71,11 @@ export default async function ServicesPage({
   // inside the route props get the dynamic url 
   const { slug } = await params;
 
-  const serviceData = (await getCachedServiceBySlug(slug)) as Service;
+  const serviceData = (await getCachedServiceBySlug(slug));
 
-  console.log("service data");
-  console.log(serviceData);
+  if (!serviceData) {
+    notFound();
+  }
 
   return (
     <div className="flex flex-col w-full items-center bg-white">
