e5294862e4ae53957143e47fe80726c9dd78381b
eleonorsilva96 <eleonor.uxui@gmail.com>
2026-03-20 21:46:57 +0000

    feat: fetch contact page data

--- app/(my-app)/contact/page.tsx
+++ app/(my-app)/contact/page.tsx
@@ -1,10 +1,10 @@
 
-// import { formatPhoneNumber } from "../lib/utils";
 import { getPayload } from "payload";
 import config from '@payload-config';
+import { unstable_cache } from "next/cache";
 
 import SectionContent from "../ui/section-content";
-import { Contact } from "@/payload-types";
+import { Metadata } from 'next'
 
 async function getContactData() {
   const payload = await getPayload({ config });
@@ -17,10 +17,35 @@ async function getContactData() {
   return result || null;
 }
 
-export default async function ContactPage() {
-  const contactData = await (getContactData()) as Contact;
+export const getCachedContactData = unstable_cache(
+  // function with the request
+  async () => getContactData(),
+  // key array to facilitate the finding of the cached data
+  ["contact-cache"],
+  // tag name to eventually clear it when data changes on the db
+  {
+    tags: ["global_contact"],
+    revalidate: 86400, // auto-revalidate every 24 hours
+  },
+);
 
-  console.log(contactData);
+export async function generateMetadata(): Promise<Metadata> {
+  const contact = await getCachedContactData();
+  
+  return {
+    title: contact.meta?.title || contact.title,
+    description: contact.meta?.description,
+    openGraph: {
+      title: contact.meta?.title || contact.title,
+      // add condition to omit description if it doesn't exist
+      ...(contact.meta?.description && { description: contact.meta.description }), 
+      images: typeof contact.meta?.image === 'object' && contact.meta?.image?.url ? [{ url: contact.meta.image.url }] : [],
+    },
+  }
+}
+
+export default async function ContactPage() {
+  const contactData = await (getCachedContactData());
 
   return (
     <div className="flex flex-col w-full items-center bg-white">
