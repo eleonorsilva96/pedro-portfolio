f5d206e7a47c5d9e1301957e702d540bc6852142
eleonorsilva96 <eleonor.uxui@gmail.com>
2026-03-20 21:45:46 +0000

    feat: fetch about me page data

--- app/(my-app)/about/page.tsx
+++ app/(my-app)/about/page.tsx
@@ -1,7 +1,7 @@
 import { getPayload } from "payload";
 import config from '@payload-config';
-
-import { AboutMe } from "../../../payload-types";
+import { unstable_cache } from "next/cache";
+import { Metadata } from 'next'
 
 import SectionContent from "../ui/section-content";
 
@@ -16,8 +16,35 @@ async function getAboutMeData() {
   return result || null;
 }
 
+export const getCachedAboutMeData = unstable_cache(
+  // function with the request
+  async () => getAboutMeData(),
+  // key array to facilitate the finding of the cached data
+  ["about-me-cache"],
+  // tag name to eventually clear it when data changes on the db
+  {
+    tags: ["global_about_me"],
+    revalidate: 86400, // auto-revalidate every 24 hours
+  },
+);
+
+export async function generateMetadata(): Promise<Metadata> {
+  const aboutMe = await getCachedAboutMeData();
+  
+  return {
+    title: aboutMe.meta?.title || aboutMe.title,
+    description: aboutMe.meta?.description,
+    openGraph: {
+      title: aboutMe.meta?.title || aboutMe.title,
+      // add condition to omit description if it doesn't exist
+      ...(aboutMe.meta?.description && { description: aboutMe.meta.description }), 
+      images: typeof aboutMe.meta?.image === 'object' && aboutMe.meta?.image?.url ? [{ url: aboutMe.meta.image.url }] : [],
+    },
+  }
+}
+
 export default async function AboutPage() {
-  const aboutData = (await getAboutMeData()) as AboutMe;
+  const aboutData = (await getCachedAboutMeData());
 
   return (
     <div className="flex flex-col w-full items-center bg-white">
